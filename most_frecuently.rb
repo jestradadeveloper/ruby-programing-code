0e407246b20f381a0f25dfd2099bf02537a071f7
Jose Estrada <webdeveloper@vgloreto.com>
2022-11-25 17:54:03 -0600

    Add Most frecuently problem solution

--- most_frecuently.rb
+++ most_frecuently.rb
@@ -5,5 +5,12 @@
 # that appears most frequently unless the array is empty.
 # For instance, you won't be given an array such as [1, 1, 2, 2].
 class MostFrecuently
-  def most_frecuent(given_list); end
+  def self.most_frecuent(list)
+    hash = {}
+    cont = 0
+    (0..list.length - 1).each do |i|
+      hash[list[i].to_i] = hash[list[i].to_i] ? cont += 1 : 1
+    end
+    hash.key hash.values.max
+  end
 end
--- spec/most_frecuently_spec.rb
+++ spec/most_frecuently_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 require_relative '../most_frecuently'
 
 describe MostFrecuently do
-  it 'Does something' do
-    expect(1).to eq(1)
+  it 'Should be found 2 like the most frecuent number in the list' do
+    list = [1, 2, 19, 5, 3, 5, 2, 10, 2]
+    expect(MostFrecuently.most_frecuent(list)).to eq(2)
   end
 end
