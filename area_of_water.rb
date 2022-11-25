84492210e00e096b932ac727bbaabb5416789af6
Jose Estrada <webdeveloper@vgloreto.com>
2022-11-25 13:00:43 -0600

    Add area of water  letcode solution

--- area_of_water.rb
+++ area_of_water.rb
@@ -1,6 +1,25 @@
+# Class to get the maximum area of heights
 class AreaOfWater
   def self.get_max_area(array)
-    (0..array.lenght - 1).each do |i|
+    res = 0
+    (0..array.length - 1).each do |l|
+      ((l + 1)..array.length - 1).each do |r|
+        area = (r - l) * [array[r], array[l]].min
+        res = [res, area].max
+      end
     end
+    res
+  end
+
+  def self.get_max_area_optimal(array)
+    l = 0
+    res = 0
+    r = (array.length - 1)
+    while l < r
+      area = (r - l) * [array[l], array[r]].min
+      res = [res, area].max
+      array[l] < array[r] ? l += 1 : r -= 1
+    end
+    res
   end
 end
--- spec/area_of_water_spec.rb
+++ spec/area_of_water_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 require_relative '../area_of_water'
 
 describe AreaOfWater do
-  it 'Should return 49' do
+  it 'Should return 49 with brute force function' do
     heights = [1, 8, 6, 2, 5, 4, 8, 3, 7]
     expect(AreaOfWater.get_max_area(heights)).to eq(49)
+  end
+  it 'Should return 49 with linear time function' do
+    heights = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    expect(AreaOfWater.get_max_area_optimal(heights)).to eq(49)
   end
 end
