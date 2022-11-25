6b83c58eb083bea8ac4c62f7916449e4b79f907d
Jose Estrada <webdeveloper@vgloreto.com>
2022-11-25 17:55:37 -0600

    Add 2 more cases on testing for AreaOfWater

--- spec/area_of_water_spec.rb
+++ spec/area_of_water_spec.rb
@@ -10,4 +10,12 @@ describe AreaOfWater do
     heights = [1, 8, 6, 2, 5, 4, 8, 3, 7]
     expect(AreaOfWater.get_max_area_optimal(heights)).to eq(49)
   end
+  it 'Should return 9 with linear time function' do
+    heights = [2, 3, 3, 2, 3]
+    expect(AreaOfWater.get_max_area_optimal(heights)).to eq(9)
+  end
+  it 'Should return 1 with linear time function' do
+    heights = [1,1]
+    expect(AreaOfWater.get_max_area_optimal(heights)).to eq(1)
+  end
 end
